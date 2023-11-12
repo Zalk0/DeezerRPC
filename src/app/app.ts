eef5603d7d2012c0f344dcc14ea3b934df9c234b
Zalko <88582103+Zalk0@users.noreply.github.com>
2023-11-12 09:38:26 +0100

    Change name to Zalko

--- src/app/app.ts
+++ src/app/app.ts
@@ -14,8 +14,8 @@ export const APP = {
     name: 'DeezerRPC',
     version: PACKAGE.version,
     homepage: PACKAGE.homepage,
-    packageUrl: 'https://raw.githubusercontent.com/Braasileiro/DeezerRPC/master/package.json',
-    appId: 'com.brasileiro.deezerrpc',
+    packageUrl: 'https://raw.githubusercontent.com/Zalk0/DeezerRPC/master/package.json',
+    appId: 'com.zalko.deezerrpc',
     settings: {
         windowWidth: 1280,
         windowHeight: 720,
